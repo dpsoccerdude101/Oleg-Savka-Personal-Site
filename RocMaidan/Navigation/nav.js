4f7c71d357fef5007cf37bc54dfc72ce87dec8eb
dpsoccerdude101 <dennispavlyuk@gmail.com>
2020-01-01 20:41:06 -0500

    About us

--- RocMaidan/Navigation/nav.js
+++ RocMaidan/Navigation/nav.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector("div.burger");
 const flexList = document.querySelector(".flex-list");
 const navLinks = document.querySelector(".nav-links");
+const header = (document.getElementsByTagName("header"))[0];
+let sticky = header.offsetTop;
 burger.addEventListener("click", () => {
   if (flexList.classList.contains("open")) {
     console.log(flexList.classList);
@@ -14,3 +16,12 @@ burger.addEventListener("click", () => {
     flexList.classList.toggle("open");
   }
 });
+window.onscroll = function() {stickyNav()};
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
