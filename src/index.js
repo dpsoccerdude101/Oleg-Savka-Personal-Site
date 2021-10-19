dd75faba4c1fa6cadd52590691689c68cf50259f
dpsoccerdude101 <dennispavlyuk@gmail.com>
2021-10-19 16:09:18 -0400

    final changes before giving up on parcel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "./components/Main.js";
 
-import { html, useEffect, component, useState } from "haunted";
+import { html, component } from "haunted";
 
 const App = () => {
-  return html` <main-component></main-component> `;
+  return html`<main-component></main-component>`;
 };
-export default App;
 customElements.define("app-page", component(App, { useShadowDOM: false }));
